27c0a8ab5e583f4c03d1f58467740ecc9e32e95b
MANAHIL ABID <soniaabid806@gmail.com>
2025-03-15 22:02:19 +0500

    Add files via upload

---
+++ index3.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>funcations</title>
+</head> 
+<body>
+    <?php
+    echo  "<h1>functions in php</h1>";
+     function weeks($day1)
+     {
+        switch($weeks){
+
+            case  $day1():
+            echo "monday:".weeks;
+            break;
+        
+            case $day2():
+            echo"tuesday:".weeks;
+            break;
+        
+            case  $day3():
+            echo"wednesday:".weeks;
+            break;
+            case  $day4():
+                echo"thursday:".weeks;
+                break;
+            
+     
+                case  $day5():
+                    echo"friday:".weeks;
+                    break;
+                
+                    case  day6():
+                   echo"saturday:".weeks;
+                    break;
+
+                        case  $day7():        
+                        echo"sunday:".weeks;
+                        break;
+                    
+             
+            default:
+            echo "the operator is invalid";
+            }
+        
+} 
+weeks("monday","tuesday","wednesday","thursday","friday","saturday","sunday");
+    ?>
+     <!-- // echo "TODAY IS:".$d1."<br>TODAY IS:".$d2.  "<br>TODAY IS:".$d3.  "<br>TODAY IS:".$d4.  "<br>TODAY IS:".$d5.  "<br>TODAY IS:".$d6.  "<br>TODAY IS:".$d7."<br>"; -->
+
+</body>
+</html>
