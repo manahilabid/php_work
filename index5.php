ce0e2aedef0b29c8e68c1df7043b066403b20ca9
MANAHIL ABID <soniaabid806@gmail.com>
2025-03-27 09:37:22 +0500

    Add files via upload

---
+++ index5.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>loops in php</title>
+</head>
+<body>
+    <?php
+
+    echo "<h3>loops in php</h3>"
+    for($e=1;$e<=10;$e++){
+        echo $e."manahil<br>";  
+
+    }
+    echo
+    
+    ?>
+</body>
+</html>
