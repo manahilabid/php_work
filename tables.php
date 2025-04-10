b6123e72d998ebb429cb621572653eec2f311e6c
MANAHIL ABID <soniaabid806@gmail.com>
2025-04-10 21:49:48 +0500

    tables by using php

---
+++ tables.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>tables</title>
+</head>
+<body>
+    <?php
+    echo "<h3>Tables</h3>";
+    echo "<table border='1'>";
+    $tableno "6";
+    for($i =1 ,$i<=10;$i++){
+    echo "<tr><td>".tableno."</td><td> x </td><td>".i."</td><td> = </td><td>".tableno*i."</td></tr>"
+}
+    echo "<table>";
+    ?>
+</body>
+</html>
